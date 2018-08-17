5e48131f2da487db9f65ae5ccf3b1ea8dec46bbf
Kaffeedrache <wiltrud.kessler@ims.uni-stuttgart.de>
2018-08-17 18:07:53 +0200

    comments for  MINT Pool update script

--- admin/poolupdate.sh
+++ admin/poolupdate.sh
@@ -21,19 +21,23 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 for file in $SCRIPTFOLDER/*.sh
 do
    if [[ -f $file ]]; then # remove the folder itself/* (happens if the folder is empty)
-   
+
       # Get the name of the file (without path)
       FILENAME="${file##*/}"
-      
-      # Get the path of the corresponding log file in the log folder
+
+      # Get the path of the corresponding log file in the log folder.
+      # The log file will have the name of the script as first part and the name
+      # of the computer it was executed on as the second part.
+      # We get the name of a computer from the environment variable $HOSTNAME
+      # which should be set on every linux.
       SEP="_"
       LOGNAME="$LOGFOLDER/$FILENAME$SEP$HOSTNAME.log"
-      
+
       # Check if there is a log file.
       # If yes, this file has already been processed, so we skip it.
       # If no, execute this file and write the result to a log in the log folder
       if [[ ! -f "$LOGNAME" ]] ; then
-      
+
          # Check if we can create the log file, if not fall back to local log file
          touch $LOGNAME
          if [[ ! -f "$LOGNAME" ]] ; then
@@ -41,10 +45,13 @@ do
            echo "I was not able to create the log file!! Logging to $LOGNAME instead"
          fi
 
+         # Execute the script file with bash, redirect outputs to log file.
+         # The -x executes the script in verbose mode, meaning every command
+         # is logged before execution.
          echo "-- process $file (logging to $LOGNAME)"
          DATE=`date +"%b %d %H:%M:%S"`
          echo "=== Processing start: $DATE" > "$LOGNAME"
-         bash -x "$file" &>> "$LOGNAME" # -x executes in verbose mode, logs commands
+         bash -x "$file" &>> "$LOGNAME"
          if [ $? -eq 1 ]
          then
             echo "=== Processing ended with Error!!!!"  >> "$LOGNAME"
@@ -52,7 +59,7 @@ do
          DATE=`date +"%b %d %H:%M:%S"`
          echo "=== Processing end: $DATE" >> "$LOGNAME"
       fi
-      
+
    fi
 done
 
