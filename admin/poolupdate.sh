d3c78022114baca8d5bbe30ef160cb2b3e7cea14
Kaffeedrache <wiltrud.kessler@ims.uni-stuttgart.de>
2018-08-17 17:59:40 +0200

    added MINT Pool update script

---
+++ admin/poolupdate.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+# Script to automatically execute updates.
+# Update Script need to be bash scripts and need to be located in $SCRIPTFOLDER.
+# A script is executed if there is no log file for this script in $LOGFOLER.
+
+# @author Wiltrud Kessler, 2017, MINT-Kolleg Stuttgart
+
+
+BASEFOLDER="/home/mintadmin/Poolsetup"
+SCRIPTFOLDER="$BASEFOLDER/updates"
+LOGFOLDER="$BASEFOLDER/logs"
+
+
+echo "Running MINT updates for '$HOSTNAME' on $SCRIPTFOLDER ..."
+
+# Set path correctly (it may not have been set when cron runs)
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Get all files in the script folder and execute each one.
+for file in $SCRIPTFOLDER/*.sh
+do
+   if [[ -f $file ]]; then # remove the folder itself/* (happens if the folder is empty)
+   
+      # Get the name of the file (without path)
+      FILENAME="${file##*/}"
+      
+      # Get the path of the corresponding log file in the log folder
+      SEP="_"
+      LOGNAME="$LOGFOLDER/$FILENAME$SEP$HOSTNAME.log"
+      
+      # Check if there is a log file.
+      # If yes, this file has already been processed, so we skip it.
+      # If no, execute this file and write the result to a log in the log folder
+      if [[ ! -f "$LOGNAME" ]] ; then
+      
+         # Check if we can create the log file, if not fall back to local log file
+         touch $LOGNAME
+         if [[ ! -f "$LOGNAME" ]] ; then
+           LOGNAME=$FILENAME$SEP$HOSTNAME.log
+           echo "I was not able to create the log file!! Logging to $LOGNAME instead"
+         fi
+
+         echo "-- process $file (logging to $LOGNAME)"
+         DATE=`date +"%b %d %H:%M:%S"`
+         echo "=== Processing start: $DATE" > "$LOGNAME"
+         bash -x "$file" &>> "$LOGNAME" # -x executes in verbose mode, logs commands
+         if [ $? -eq 1 ]
+         then
+            echo "=== Processing ended with Error!!!!"  >> "$LOGNAME"
+         fi
+         DATE=`date +"%b %d %H:%M:%S"`
+         echo "=== Processing end: $DATE" >> "$LOGNAME"
+      fi
+      
+   fi
+done
+
+echo "... done running MINT updates!"
